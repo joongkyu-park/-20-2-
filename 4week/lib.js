5f8f404974218bdd9ad82309200994495e62f981
joongkyu-park <jk.park.tech@gmail.com>
2020-10-06 01:13:35 +0900

    인터넷 프로그래밍 커밋

---
+++ 4week/lib.js
@@ -0,0 +1,7 @@
+/* 자바스크립트 파일 lib.js */
+function over(obj) {
+	obj.src="https://healthtips.co.kr/wp-content/uploads/2018/11/%EB%B0%94%EB%82%98%EB%82%98-%EC%B9%BC%EB%A1%9C%EB%A6%AC.jpg";
+}
+function out(obj) {
+	obj.src="https://lh3.googleusercontent.com/proxy/l7wi7G-Kwzgw11Jwijv0uHFFf7Tj3QggGw8mmunwobqq3MzHUNsY1aYpFcMspzZN_uHrmfwVlL7z3e4veAdQNb_mLVBc8zVlXcORe_Fnr_AyCYHX4r1TlIeTLbLxeaCyCp8CJAcZOSAY1E0c45THe3xwjAuPVER73JBhG3IGk5vDAsl5svqUE6F5t-2eiuIzZAG4TyyjWu0czXIM2oB_XLcysagEFfVaUvY9Av3gnRwKNuMQ-n3tDfweEaf788MvU9vYDfkenuccxeQ3pOKlTyZlVEL4Yxm92MBG7_9HTM6XqufrgKXKBy1zECw";
+}
